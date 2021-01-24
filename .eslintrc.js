833201d297a93bd6ecccc4fb69b75c8083d5e731
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2021-01-25 00:33:28 +0800

    feat: 调整语法规则和代码风格

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,110 @@
+
+/**
+ * ESLint 是一个插件化并且可配置的 JavaScript 语法规则和代码风格的检查工具。
+ * 参考文档
+ * 【eslint英文文档】https://eslint.org/docs/user-guide/configuring
+ * 【eslint中文文档】http://eslint.cn/docs/rules/
+ */
+
+/**
+ * eslint有三种使用方式
+ * 【1】js代码中通过注释的方式使用
+ * 【2】通过webpack的eslintConfig字段设置，eslint会自动搜索项目的package.json文件中的配置
+ * 【3】通过配置文件的方式使用，配置文件有多种文件方式，如JavaScript、JSON 或者 YAML等
+ */
+
+/**
+ * 文件忽略
+ * 【】让eslint跳过特定文件的检测
+ * 【】通过当前工作目录下 「.eslintignore」 文件进行设置
+ *  其使用的是Glob路径书写方式，与「.gitignore」的使用方法相同
+ * 【】也可以在 package.json 文件中，通过 eslintIgnore 参数进行设置
+ */
+
+/**
+ * 文件内局部设置
+ * 【】eslint可以具体文件中设置特定代码的规则，常用于跳过某条语句的检测。
+ * 【】注销全部规则，在代码前新建一行，添加注销 /* eslint-disable *\/  。如果没有恢复设置的语句，则下列全部代码都会跳过检测。
+ * 【】恢复eslint，在代码前新建一行，添加注销 /* eslint-enable *\/
+ * 【】指定忽略的规则，/* eslint-disable no-alert, no-console *\/
+ * 【】在特定行禁用，// eslint-disable-line
+ * 【】在下一行禁用，// eslint-disable-next-line
+ */
 module.exports = {
-  root: true,
+  root: true, // 标识当前配置文件为eslint的根配置文件，让其停止在父级目录中继续寻找
+  /**
+  * 解析器配置项
+  * http://eslint.cn/docs/user-guide/configuring#specifying-parser-options
+  *  #这里设置的配置项将会传递到解析器中，被解析器获取到，进行一定的处理。具体被利用到，要看解析器的源码有没有对其进行利用。这里仅仅做了参数定义，做了规定，告诉解析器的开发者可能有这些参数
+  *  #配置项目有：
+  * "sourceType": "module",  // 指定JS代码来源的类型，script(script标签引入？) | module（es6的module模块），默认为script。为什么vue的会使用script呢？因为vue是通过babel-loader编译的，而babel编译后的代码就是script方式
+  * "ecmaVersion": 6,     // 支持的ES语法版本，默认为5。注意只是语法，不包括ES的全局变量。全局变量需要在env选项中进行定义
+  * "ecmaFeatures": {     // Features是特征的意思，这里用于指定要使用其他那些语言对象
+    "experimentalObjectRestSpread": true, //启用对对象的扩展
+    "jsx": true,              //启用jsx语法
+    "globalReturn":true,          //允许return在全局使用
+    "impliedStrict":true          //启用严格校验模式
+   }
+  */
   parserOptions: {
+    /**
+    * 解析器
+    * http://eslint.cn/docs/user-guide/configuring#specifying-parser
+    * #ESLint 默认使用Espree作为其解析器
+    * #解析器必须是本地安装的一个 npm 模块。即必须按照在本地的node_module中
+    * #解析器是用于解析js代码的，怎么去理解每一个表达式，有点C++中编译器的概念，会对js进行一些语法分析，语义分析什么的，才能判断语句符不符合规范。而不是通过简单的字符串对比。
+    * #解析器有很多，但兼容eslint的解析器有以下几个
+    * Espree：默认解析器，一个从Esprima中分离出来的解析器，做了一些优化
+    * Esprima：js标准解析器
+    * Babel-ESLint： 一个对Babel解析器的包装，babel本身也是js解析器的一种（不然怎么能转化为兼容性代码呢？首先需要进行js解析，才能转化）。如果我们的代码需要经过babel转化，则对应使用这个解析器
+    * typescript-eslint-parser(实验) - 一个把 TypeScript 转换为 ESTree 兼容格式的解析器
+    */
     parser: 'babel-eslint',
     sourceType: 'module'
   },
+  /**
+  * 运行环境
+  * http://eslint.cn/docs/user-guide/configuring#specifying-environments
+  *  #一个环境定义了一组预定义的全局变量
+  *  #获得了特定环境的全局定义，就不会认为是开发定义的，跳过对其的定义检测。否则会被认为改变量未定义
+  *  #常见的运行环境有以下这些，更多的可查看官网
+  * browser - 浏览器环境中的全局变量。
+  * node - Node.js 全局变量和 Node.js 作用域。
+  * es6 - 启用除了 modules 以外的所有 ECMAScript 6 特性（该选项会自动设置 ecmaVersion 解析器选项为 6）。
+  * amd - 将 require() 和 define() 定义为像 amd 一样的全局变量。
+  * commonjs - CommonJS 全局变量和 CommonJS 作用域 (用于 Browserify/WebPack 打包的只在浏览器中运行的代码)。
+  * jquery - jQuery 全局变量。
+  * mongo - MongoDB 全局变量。
+  * worker - Web Workers 全局变量。
+  * serviceworker - Service Worker 全局变量。
+  */
   env: {
     browser: true,
     node: true,
     es6: true,
   },
+  /**
+  * 规则继承
+  * http://eslint.cn/docs/user-guide/configuring#extending-configuration-files
+  * #可继承的方式有以下几种
+  * #eslint内置推荐规则，就只有一个，即「eslint:recommended」
+  * #可共享的配置， 是一个 npm 包，它输出一个配置对象。即通过npm安装到node_module中
+  *  可共享的配置可以省略包名的前缀 eslint-config-，即实际设置安装的包名是 eslint-config-airbnb-base
+  * #从插件中获取的规则，书写规则为 「plugin:插件包名/配置名」，其中插件报名也是可以忽略「eslint-plugin-」前缀。如'plugin:vue/essential'
+  * #从配置文件中继承，即继承另外的一个配置文件，如'./node_modules/coding-standard/eslintDefaults.js'
+  */
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
-
-  // add your custom rules here
-  //it is base on https://github.com/vuejs/eslint-config-vue
+  /**
+  * 自定义规则
+  * http://eslint.cn/docs/user-guide/configuring#configuring-rules
+  * #基本使用方式
+  * "off" 或者0 关闭规则
+  * "warn" 或者1 将规则打开为警告（不影响退出代码）
+  * "error" 或者2 将规则打开为错误（触发时退出代码为1）
+  * 如：'no-restricted-syntax': 0, // 表示关闭该规则
+  * #如果某项规则，有额外的选项，可以通过数组进行传递，而数组的第一位必须是错误级别。如0,1,2
+  * 如 'semi': ['error', 'never'], never就是额外的配置项
+  */
   rules: {
     "vue/max-attributes-per-line": [2, {
       "singleline": 10,
@@ -47,7 +139,8 @@ module.exports = {
     'curly': [2, 'multi-line'],
     'dot-location': [2, 'property'],
     'eol-last': 2,
-    'eqeqeq': ["error", "always", {"null": "ignore"}],
+    'eqeqeq': ['off'], // 不需要强制使用全等
+    // 'eqeqeq': ["error", "always", {"null": "ignore"}], // == 强制转换 ===
     'generator-star-spacing': [2, {
       'before': true,
       'after': true
@@ -166,7 +259,8 @@ module.exports = {
       'avoidEscape': true,
       'allowTemplateLiterals': true
     }],
-    'semi': [2, 'never'],
+    'semi': 0, // 不检查分号
+    // 'semi': [2, 'never'], // 语句结尾不加分号；"semi": [2, "always"],//语句结尾强制加分号
     'semi-spacing': [2, {
       'before': false,
       'after': true
